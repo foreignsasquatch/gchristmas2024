06e8a4c34d4de2911c4f0c072f0e1949decc987c
Ratul Krisna <ratul.krisna01@protonmail.com>
2024-12-13 22:53:56 +0530

    just a window with imgui demo

---
+++ src/game.h
@@ -0,0 +1,8 @@
+typedef struct {
+  int window_width;
+  int window_height;
+  int target_framerate;
+  int update_rate;
+  float update_step;
+  float update_counter;
+} GameData;
---
+++ src/main.cpp
@@ -0,0 +1,131 @@
+#include "raylib.h"
+#include "game.h"
+
+#define IMGUI_ENABLE_FREETYPE
+#include "imgui.h"
+#include "imgui_impl_raylib.h"
+
+// imgui style
+// copied from the internet
+struct ImVec3 { float x, y, z; ImVec3(float _x = 0.0f, float _y = 0.0f, float _z = 0.0f) { x = _x; y = _y; z = _z; } };
+void imgui_easy_theming(ImVec3 color_for_text, ImVec3 color_for_head, ImVec3 color_for_area, ImVec3 color_for_body, ImVec3 color_for_pops) {
+    ImGuiStyle& style = ImGui::GetStyle();
+
+    style.WindowRounding = 8;
+    style.TabRounding = 8;
+    style.GrabRounding = 8;
+    style.ChildRounding = 8;
+    style.FrameRounding = 8;
+    style.ScrollbarRounding = 8;
+    style.PopupRounding = 8;
+
+    style.Colors[ImGuiCol_Text] = ImVec4( color_for_text.x, color_for_text.y, color_for_text.z, 1.00f );
+    style.Colors[ImGuiCol_TextDisabled] = ImVec4( color_for_text.x, color_for_text.y, color_for_text.z, 0.58f );
+    style.Colors[ImGuiCol_WindowBg] = ImVec4( color_for_body.x, color_for_body.y, color_for_body.z, 0.95f );
+    style.Colors[ImGuiCol_ChildBg] = ImVec4( color_for_area.x, color_for_area.y, color_for_area.z, 0.58f );
+    style.Colors[ImGuiCol_Border] = ImVec4( color_for_body.x, color_for_body.y, color_for_body.z, 0.00f );
+    style.Colors[ImGuiCol_BorderShadow] = ImVec4( color_for_body.x, color_for_body.y, color_for_body.z, 0.00f );
+    style.Colors[ImGuiCol_FrameBg] = ImVec4( color_for_area.x, color_for_area.y, color_for_area.z, 1.00f );
+    style.Colors[ImGuiCol_FrameBgHovered] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 0.78f );
+    style.Colors[ImGuiCol_FrameBgActive] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 1.00f );
+    style.Colors[ImGuiCol_TitleBg] = ImVec4( color_for_area.x, color_for_area.y, color_for_area.z, 1.00f );
+    style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4( color_for_area.x, color_for_area.y, color_for_area.z, 0.75f );
+    style.Colors[ImGuiCol_TitleBgActive] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 1.00f );
+    style.Colors[ImGuiCol_MenuBarBg] = ImVec4( color_for_area.x, color_for_area.y, color_for_area.z, 0.47f );
+    style.Colors[ImGuiCol_ScrollbarBg] = ImVec4( color_for_area.x, color_for_area.y, color_for_area.z, 1.00f );
+    style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 0.21f );
+    style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 0.78f );
+    style.Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 1.00f );
+    style.Colors[ImGuiCol_CheckMark] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 0.80f );
+    style.Colors[ImGuiCol_SliderGrab] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 0.50f );
+    style.Colors[ImGuiCol_SliderGrabActive] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 1.00f );
+    style.Colors[ImGuiCol_Button] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 0.50f );
+    style.Colors[ImGuiCol_ButtonHovered] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 0.86f );
+    style.Colors[ImGuiCol_ButtonActive] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 1.00f );
+    style.Colors[ImGuiCol_Header] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 0.76f );
+    style.Colors[ImGuiCol_HeaderHovered] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 0.86f );
+    style.Colors[ImGuiCol_HeaderActive] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 1.00f );
+    //style.Colors[ImGuiCol_Column] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 0.32f );
+    //style.Colors[ImGuiCol_ColumnHovered] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 0.78f );
+    //style.Colors[ImGuiCol_ColumnActive] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 1.00f );
+    style.Colors[ImGuiCol_ResizeGrip] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 0.15f );
+    style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 0.78f );
+    style.Colors[ImGuiCol_ResizeGripActive] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 1.00f );
+    //style.Colors[ImGuiCol_CloseButton] = ImVec4( color_for_text.x, color_for_text.y, color_for_text.z, 0.16f);
+    style.Colors[ImGuiCol_PlotLines] = ImVec4( color_for_text.x, color_for_text.y, color_for_text.z, 0.63f );
+    style.Colors[ImGuiCol_PlotLinesHovered] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 1.00f );
+    style.Colors[ImGuiCol_PlotHistogram] = ImVec4( color_for_text.x, color_for_text.y, color_for_text.z, 0.63f );
+    style.Colors[ImGuiCol_PlotHistogramHovered] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 1.00f );
+    style.Colors[ImGuiCol_TextSelectedBg] = ImVec4( color_for_head.x, color_for_head.y, color_for_head.z, 0.43f );
+    style.Colors[ImGuiCol_PopupBg] = ImVec4( color_for_pops.x, color_for_pops.y, color_for_pops.z, 0.92f );
+    style.Colors[ImGuiCol_ModalWindowDimBg] = ImVec4( color_for_area.x, color_for_area.y, color_for_area.z, 0.73f );
+}
+
+void setupImGuiStyle() {
+    static ImVec3 color_for_text = ImVec3(236.f / 255.f, 240.f / 255.f, 241.f / 255.f);
+    static ImVec3 color_for_head = ImVec3(41.f / 255.f, 128.f / 255.f, 185.f / 255.f);
+    static ImVec3 color_for_area = ImVec3(57.f / 255.f, 79.f / 255.f, 105.f / 255.f);
+    static ImVec3 color_for_body = ImVec3(44.f / 255.f, 62.f / 255.f, 80.f / 255.f);
+    static ImVec3 color_for_pops = ImVec3(33.f / 255.f, 46.f / 255.f, 60.f / 255.f);
+    imgui_easy_theming(color_for_text, color_for_head, color_for_area, color_for_body, color_for_pops);
+}
+
+int main() {
+  GameData data;
+  data.window_width = 1920;
+  data.window_height = 1080;
+  data.target_framerate = 60;
+  data.update_rate = 60;
+  data.update_step = 1.0f / 60.0f;
+  data.update_counter = 0.0f;
+
+  SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI | FLAG_FULLSCREEN_MODE);
+  SetTraceLogLevel(LOG_ERROR);
+
+  InitWindow(data.window_width, data.window_height, "gchristmas2024");
+  SetTargetFPS(data.target_framerate);
+  SetExitKey(KEY_NULL);
+
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO(); (void)io;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+  ImGui::StyleColorsDark();
+
+  ImGui_ImplRaylib_Init();
+
+  ImFont* font = io.Fonts->AddFontFromFileTTF("../content/debug.ttf", 18);
+  IM_ASSERT(font != nullptr);
+
+  ImGui_ImplRaylib_BuildFontAtlas();
+
+  setupImGuiStyle();
+
+  while(!WindowShouldClose()) {
+    ImGui_ImplRaylib_ProcessEvents();
+    ImGui_ImplRaylib_NewFrame();
+
+    data.update_counter += GetFrameTime();
+    while(data.update_counter > data.update_step) {
+      // update
+      data.update_counter -= data.update_step;
+    }
+
+    BeginDrawing();
+    ImGui::NewFrame();
+
+    ClearBackground(BLACK);
+
+    // draw
+    
+    bool open = true;
+    ImGui::ShowDemoWindow(&open);
+
+    ImGui::Render();
+    ImGui_ImplRaylib_RenderDrawData(ImGui::GetDrawData());
+    EndDrawing();
+  }
+
+  ImGui_ImplRaylib_Shutdown();
+  ImGui::DestroyContext();
+  CloseWindow();
+}
